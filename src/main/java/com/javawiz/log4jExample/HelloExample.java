a5951e959674b21fda25eaf3868852d229d3b1b0
Ranjan Sai <mail2javawiz@gmail.com>
2017-04-19 13:17:13 +0530

    change in main class

--- src/main/java/com/javawiz/log4jExample/HelloExample.java
+++ src/main/java/com/javawiz/log4jExample/HelloExample.java
@@ -7,27 +7,28 @@ public class HelloExample{
 
 	public static void main(String[] args) {
 
-		HelloExample obj = new HelloExample();
-		obj.runMe("log4j");
-		String creditCardNum = "1234567812341111";
-    	logger.info("Credit Card : " + creditCardNum);
+		String card = "Aaaa bbbb aaa=1234567890123456 fdfdfd=aaa";
+		logger.info("Credit Card : " + card);
 
+	    String card1 = "Aaaa bbbb aaa=\"12345678901234561\" fdfdfd=aaa";
+	    logger.info("Credit Card : " + card1);
+	    
+	    String card2 = "Aaaa bbbb aaa=1gfg23fgfg4567123123fdfdfd=aaa";
+	    logger.info("Credit Card : " + card2);
+	    
+		String card3 = "<cardNumber>1234567812345678</cardNumber>";
+    	logger.info("Credit Card : " + card3);
+    	
+    	String pan = "Aaaa bbbb aaa=1234567890 fdfdfd=aaa";
+    	logger.info("PAN : " + pan);
+    	
+    	String pan1 = "Aaaa bbbb aaa=\"1234567890\" fdfdfd=aaa";
+    	logger.info("PAN : " + pan1);
+    	
+    	String pan2 = "<accountNumber>7812345678</accountNumber>";
+    	logger.info("PAN : " + pan2);
+    	
+    	String pan3 ="6754265443";
+    	logger.info("PAN : " + pan3);
 	}
-
-	private void runMe(String parameter){
-
-		if(logger.isDebugEnabled()){
-			logger.debug("This is debug : " + parameter);
-		}
-
-		if(logger.isInfoEnabled()){
-			logger.info("This is info : " + parameter);
-		}
-
-		logger.warn("This is warn : " + parameter);
-		logger.error("This is error : " + parameter);
-		logger.fatal("This is fatal : " + parameter);
-
-	}
-
 }
